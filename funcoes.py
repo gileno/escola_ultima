8124e721fecf93551baafbfb257788c013140655
Gileno Filho <contato@gilenofilho.com.br>
2022-11-22 20:40:41 -0300

    adicionando função para buscar_aluno e funcionalidade para atualizar aluno

--- funcoes.py
+++ funcoes.py
@@ -12,3 +12,15 @@ def consulta_aluno(cursor):
 
     for aluno in resultado:
         print("ID", aluno[0], "Nome", aluno[1], "Email", aluno[2], "Data", aluno[3])
+    
+
+def buscar_aluno(cursor, id):
+    sql = '''
+    select id, nome, email, data from aluno where id = ?
+    '''
+    resultado = cursor.execute(sql, [id])
+
+    aluno = None
+    for aluno in resultado:
+        break
+    return aluno
